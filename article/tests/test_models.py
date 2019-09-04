26514d6efca8c09868ad25a1421e22bf381ce268
Bakyt <isimovabakyt@gmail.com>
2019-09-04 14:02:50 +0600

    flake8 used

--- article/tests/test_models.py
+++ article/tests/test_models.py
@@ -7,22 +7,26 @@ class CompanyTest(TestCase):
     def setUp(self):
         Company.objects.create(name='EGOV')
         Company.objects.create(name='ArticleReport')
-    
+
     def test_company_str(self):
-        egov=Company.objects.get(name='EGOV')
-        artreport=Company.objects.get(name='ArticleReport')
+        egov = Company.objects.get(name='EGOV')
+        artreport = Company.objects.get(name='ArticleReport')
         self.assertEqual(egov.__str__(), "EGOV")
         self.assertEqual(artreport.__str__(), "ArticleReport")
 
 
 class ArticleTest(TestCase):
     def setUp(self):
-        egov=Company.objects.create(name='EGOV')
-        author=User.objects.create(username='bakyt', email='isimovabakyt@gmail.com', password='ghbdtn1234')
-        Article.objects.create(title='Waterpost', rating=4, summary='djfvjnadvk;oafjnv', 
-        ip_address='127.0.0.4', submission_date='2017-07-15', company=egov, reviewer=author)
-    
+        egov = Company.objects.create(name='EGOV')
+        author = User.objects.create(username='bakyt',
+                                     email='isimovabakyt@gmail.com',
+                                     password='ghbdtn1234')
+        Article.objects.create(title='Waterpost', rating=4,
+                               summary='djfvjnadvk;oafjnv',
+                               ip_address='127.0.0.4',
+                               submission_date='2017-07-15',
+                               company=egov, reviewer=author)
+
     def test_article_str(self):
-        article=Article.objects.get(title='Waterpost')
-        self.assertEqual(article.__str__(), ("{} Waterpost").format(str(article.id)))
-        
+        article = Article.objects.get(title='Waterpost')
+        self.assertEqual(article.__str__(), ("{} Waterpost").format(str(article.id)))  # noqa
--- article/tests/test_views.py
+++ article/tests/test_views.py
@@ -1,37 +1,34 @@
 from article.models import Company
 from django.contrib.auth.models import User
-from django.urls import reverse
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient, APITestCase
 
 
 class ArticleTestApi(APITestCase):
     def setUp(self):
-        self.client=APIClient()
-        self.user=User.objects.create(username='bakyt', email='isimovabakyt@gmail.com', 
-        password='ghbdtn1234')
-        self.user.save()
-        #self.tokenNew= Token.objects.get_or_create(user=self.user)[0]
-        #print(Token.objects.all())
-        response=self.client.post('/api-token-auth/', 
-        {"username":"bakyt", "password": "ghbdtn1234"})
-        self.token=response.data
-        self.company=Company.objects.create(name='print')
+        self.client = APIClient()
+        self.user = User.objects.create(username='bakyt',
+                                        email='isimovabakyt@gmail.com',
+                                        password='ghbdtn1234')
+        response = self.client.post(
+            '/api-token-auth/',
+            {"username": "bakyt", "password": "ghbdtn1234"})
+        self.token = response.data
+        self.company = Company.objects.create(name='print')
 
     def test_list(self):
-        print(self.token)
-        response=self.client.get('/api/article/', Authorization='Token {self.token}')
-        #print(response.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        response = self.client.get('/api/article/',
+                                   Authorization='Token {self.token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create(self):
-        data={
+        data = {
             'title': 'testPost',
             'rating': 1,
             'summary': 'ghbdtn',
             'submission_date': '2017-07-12',
             'company': 1
         }
-        response = self.client.post('/api/article/', data, Authorization='Token {self.token}')
+        response = self.client.post('/api/article/', data,
+                                    Authorization='Token {self.token}')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
